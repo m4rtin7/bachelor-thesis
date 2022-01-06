66425b7b10f3a1acc16b6d0be818dc5ebae5f343
m4rtin7 <m.zavadzan@gmail.com>
2022-01-06 11:31:52 +0100

    docker container for testing

---
+++ backend/docker/1_characterType.cpp
@@ -0,0 +1,6 @@
+#include "solution2.h"
+
+bool jeParne(int i)
+{
+    return i % 2 == 0;
+}
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,34 @@
 require("dotenv").config();
 const express = require("express");
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
 const app = express();
 
 app.get("/", (_req, res) => {
-  res.send("Hello");
+  const p = path.resolve(__dirname, "./docker/fail.txt");
+  exec(
+    "cd docker && sudo docker build --rm . -t first-docker:1 && timeout 30 sudo docker run --rm first-docker:1 > fail.txt && sudo docker image prune -f",
+    (error, stdout, stderr) => {
+      if (error) {
+        res.sendFile(p);
+
+        console.log(`error: ${error}`);
+        return;
+      }
+      if (stderr) {
+        res.sendFile(p);
+
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      // console.log(`stdout: ${stdout}`);
+
+      const data = fs.readFileSync(p, "utf8");
+      res.sendFile(p);
+    }
+  );
 });
 
 const port = process.env.PORT || 3001;
